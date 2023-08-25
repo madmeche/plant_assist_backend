2e8ca7b1e26f0114762761e561bac54ab0ca227b
madmeche <madeleine.meche@gmail.com>
2023-08-25 14:26:26 -0500

    re uploading

--- Backend/server.js
+++ Backend/server.js
@@ -1,54 +1,66 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 require("dotenv").config();
 
-let dbconnect = require('./dbConnect');
+let dbconnect = require("./dbConnect");
 
-const Models = require('./models');
+const Models = require("./models");
 // const Seeds = require('./seeds');
 
-
 async function init() {
-    await Models.User.sync();
-    await Models.Plant.sync();
-    await Models.Folder.sync()
-    await Models.Favorite.sync()
-    await Models.Recent.sync()
-  } 
-  init();
+  await Models.User.sync();
+  await Models.Plant.sync();
+  await Models.Folder.sync();
+  await Models.Favorite.sync();
+  await Models.Recent.sync();
+}
+init();
 
-const authRoutes = require('./routes/authRoutes')
-const userRoutes = require('./routes/userRoutes')
-const plantRoutes = require('./routes/plantRoutes')
-const folderRoutes = require('./routes/foldersRoutes')
-const favoritesRoutes = require('./routes/favoritesRoutes')
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const plantRoutes = require("./routes/plantRoutes");
+const folderRoutes = require("./routes/foldersRoutes");
+const favoritesRoutes = require("./routes/favoritesRoutes");
 
 // const seedPlant = require('./seeds/seedPlant')
 // seedPlant.seedPlant()
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.setHeader("Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With, content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/plant', plantRoutes)
-app.use('/api/folder', folderRoutes)
-app.use('/api/favrotie', favoritesRoutes)
+const cors=require("cors");
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
+// app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/plant", plantRoutes);
+app.use("/api/folder", folderRoutes);
+app.use("/api/favorite", favoritesRoutes);
 
 app.get("/", (req, res) => {
-    res.send({message: "Welcome to Plant Assist"});
+  res.send({ message: "Welcome to Plant Assist" });
 });
 
 // app.post('/users', (req, res) => {
@@ -60,5 +72,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
